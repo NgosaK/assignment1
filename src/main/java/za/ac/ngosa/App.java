73c01c49385742d895a10b27234741349b82af4f
NgosaK <kangwa.ngosa@yahoo.com>
2015-09-14 00:09:15 +0200

    Initial commit

---
+++ src/main/java/za/ac/ngosa/App.java
@@ -0,0 +1,13 @@
+package za.ac.ngosa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
